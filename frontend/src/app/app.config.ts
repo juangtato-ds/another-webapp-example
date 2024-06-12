e05a5ec0fbbebd88b4aa88d838f038fa3dde1410
Juan <juan@dataspartan.com>
2024-06-12 15:34:36 +0200

    feat(web): scss

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,15 +2,15 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     // Basic
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations(),
+    provideAnimationsAsync(),
     provideHttpClient(),
   ]
 };
