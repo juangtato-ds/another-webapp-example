899a7f6f47b66f11fad67318afa0ee30828acce1
Juan <juan@dataspartan.com>
2024-06-12 17:13:41 +0200

    fix(back): country typo

--- backend/src/song_backend_rest/model/song.py
+++ backend/src/song_backend_rest/model/song.py
@@ -17,6 +17,6 @@ class SongFullResponse(BaseModel):
     artist: str = Field(description="Artist name", examples=["Rainbow"])
     title: str = Field(description="Title of the song", examples=["The Temple of the King"])
     lyrics: str = Field(description="Song lyrics", examples=["Lorem ipsum dolor"])
-    contry_list: list[str] = Field(
+    country_list: list[str] = Field(
         description="List of countries in the song", examples=[["Narnia", "Mordor"]]
     )
