4e0016f6c63ff8f9874f1cfcfbfca57acffc2fb3
Juan <juan@dataspartan.com>
2024-06-13 10:27:25 +0200

    feat(web): recover song data when creation error

--- frontend/src/app/module/song/abstract-add-song.component.ts
+++ frontend/src/app/module/song/abstract-add-song.component.ts
@@ -3,24 +3,18 @@ import { MatDialog } from "@angular/material/dialog";
 import { ActivatedRoute, Router } from "@angular/router";
 import { SongService } from "./song.service";
 import { SongFormDialogComponent } from "./song-form-dialog/song-form-dialog.component";
+import { SongForm } from "./model/song.model";
 
 export abstract class AbstractAddSongComponent {
+    private previousData?: SongForm;
+
     protected songService = inject(SongService);
     protected router = inject(Router);
     protected activatedRouter = inject(ActivatedRoute);
     protected dialog = inject(MatDialog);
 
     add(): void {
-        const dialogRef = this.dialog.open(SongFormDialogComponent, { data: {} });
-        dialogRef.afterClosed().subscribe(result => {
-            if (result) {
-                this.songService.create(result).subscribe({
-                    next: s => this.router.navigate([s.id], { relativeTo: this.activatedRouter }),
-                    error: this.processError
-                });
-                // TODO loading animation
-            }
-        });
+        this.showAddModal();
     }
 
     private processError(e: any): void {
@@ -32,6 +26,21 @@ export abstract class AbstractAddSongComponent {
         } else {
             alert('Unexpected error');
         }
+        this.showAddModal(this.previousData);
+    }
+
+    private showAddModal(song?: SongForm): void {
+        this.previousData = song;
+        const dialogRef = this.dialog.open(SongFormDialogComponent, { data: { song } });
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.songService.create(result).subscribe({
+                    next: s => this.router.navigate([s.id], { relativeTo: this.activatedRouter }),
+                    error: this.processError
+                });
+                // TODO loading animation
+            }
+        });
     }
 
 }
